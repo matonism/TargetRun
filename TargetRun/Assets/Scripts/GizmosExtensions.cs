4204cdc279615000028ed9afa41341aba8f5d2d8
John Jackson <jackson.2028@osu.edu>
2016-04-21 18:37:11 -0400

    Added object spawner.

---
+++ TargetRun/Assets/Scripts/GizmosExtensions.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public static class GizmosExtension
+{
+    public static void DrawCube(Vector3 lowerFrontLeft, Vector3 upperBackRight, Color color)
+    {
+        Gizmos.color = color;
+
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, lowerFrontLeft.y, lowerFrontLeft.z), new Vector3(upperBackRight.x, lowerFrontLeft.y, lowerFrontLeft.z));
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, lowerFrontLeft.y, lowerFrontLeft.z), new Vector3(lowerFrontLeft.x, lowerFrontLeft.y, upperBackRight.z));
+        Gizmos.DrawLine(new Vector3(upperBackRight.x, lowerFrontLeft.y, lowerFrontLeft.z), new Vector3(upperBackRight.x, lowerFrontLeft.y, upperBackRight.z));
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, lowerFrontLeft.y, upperBackRight.z), new Vector3(upperBackRight.x, lowerFrontLeft.y, upperBackRight.z));
+
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, upperBackRight.y, lowerFrontLeft.z), new Vector3(upperBackRight.x, upperBackRight.y, lowerFrontLeft.z));
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, upperBackRight.y, lowerFrontLeft.z), new Vector3(lowerFrontLeft.x, upperBackRight.y, upperBackRight.z));
+        Gizmos.DrawLine(new Vector3(upperBackRight.x, upperBackRight.y, lowerFrontLeft.z), new Vector3(upperBackRight.x, upperBackRight.y, upperBackRight.z));
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, upperBackRight.y, upperBackRight.z), new Vector3(upperBackRight.x, upperBackRight.y, upperBackRight.z));
+
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, lowerFrontLeft.y, lowerFrontLeft.z), new Vector3(lowerFrontLeft.x, upperBackRight.y, lowerFrontLeft.z));
+        Gizmos.DrawLine(new Vector3(lowerFrontLeft.x, lowerFrontLeft.y, upperBackRight.z), new Vector3(lowerFrontLeft.x, upperBackRight.y, upperBackRight.z));
+        Gizmos.DrawLine(new Vector3(upperBackRight.x, lowerFrontLeft.y, lowerFrontLeft.z), new Vector3(upperBackRight.x, upperBackRight.y, lowerFrontLeft.z));
+        Gizmos.DrawLine(new Vector3(upperBackRight.x, lowerFrontLeft.y, upperBackRight.z), new Vector3(upperBackRight.x, upperBackRight.y, upperBackRight.z));
+    }
+
+}
---
+++ TargetRun/Assets/Scripts/ObstacleSpawner.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleSpawner : MonoBehaviour {
+
+    public Vector3 Rotation;
+    public Vector3 Center;
+    public Vector3 Size;
+    public bool Randomize;
+    public Vector3 SpawnPosition;
+    public GameObject[] SpawnTypes;
+
+	void Start () {
+        Quaternion rotation = Quaternion.Euler(Rotation);
+        Vector3 halfSize = Size / 2.0f;
+        Vector3 right = (rotation * Vector3.right);
+        right.Normalize();
+        Vector3 up = (rotation * Vector3.up);
+        up.Normalize();
+        Vector3 forward = (rotation * Vector3.forward);
+        forward.Normalize();
+
+        Vector3 rotatedHalfSize = right * halfSize.x + up * halfSize.y + right * halfSize.z;
+        Vector3 lfl = transform.position + Center - rotatedHalfSize;
+        Vector3 ubr = transform.position + Center + rotatedHalfSize;
+
+        if (Randomize) { SpawnPosition = new Vector3(Random.Range(lfl.x, ubr.x), Random.Range(lfl.y, ubr.y), Random.Range(lfl.z, ubr.z)); }
+        var spawn = SpawnTypes[Random.Range(0, SpawnTypes.Length)];
+        GameObject.Instantiate(spawn, SpawnPosition, this.transform.rotation * spawn.transform.rotation);
+        GameObject.Destroy(this);
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position + Center + SpawnPosition, 0.25f);
+
+        Quaternion rotation = Quaternion.Euler(Rotation);
+        Vector3 halfSize = Size / 2.0f;
+        Vector3 right = (rotation * Vector3.right);
+        right.Normalize();
+        Vector3 up = (rotation * Vector3.up);
+        up.Normalize();
+        Vector3 forward = (rotation * Vector3.forward);
+        forward.Normalize();
+
+        Vector3 rotatedHalfSize = right * halfSize.x + up * halfSize.y + right * halfSize.z;
+        Vector3 lfl = transform.position + Center - rotatedHalfSize;
+        Vector3 ubr = transform.position + Center + rotatedHalfSize;
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(lfl, 0.25f);
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(ubr, 0.25f);
+        GizmosExtension.DrawCube(lfl, ubr, Color.red);
+    }
+
+}
